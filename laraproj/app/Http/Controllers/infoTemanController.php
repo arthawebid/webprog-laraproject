35d04d714b7326931903ae0b48f4439836854680
MhSTIKI <arthawebid@gmail.com>
2025-10-22 17:55:23 +0800

    update data teman

--- laraproj/app/Http/Controllers/infoTemanController.php
+++ laraproj/app/Http/Controllers/infoTemanController.php
@@ -8,6 +8,24 @@ class infoTemanController extends Controller
 {
     public function index(){
         $nama = "Zizuki";
-        return view('info', compact('nama') );
+        
+        $dtateman = [
+            [
+                "namateman"=>"Tangidah Idun",
+                "alamat"=>"Jl. Persatuan Kodok Blok F No 70",
+                "kota"=>"Denpasar",
+                'wa'=>"086677889",
+                'idbuku'=>1,
+            ],
+            [    
+                "namateman"=>"Rudiani Putri Dimana",
+                "alamat"=>"Jl. Tegak Lurus Blok T No 80",
+                "kota"=>"Denpasar",
+                "wa"=>"08774433267",
+                "idbuku"=>2,
+            ],
+        ];
+
+        return view('info', compact('nama','dtateman') );
     }
 }
