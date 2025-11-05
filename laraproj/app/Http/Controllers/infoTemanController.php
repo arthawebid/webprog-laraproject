6457302c94114e9aaef940cb7d9b19e4bc5455a7
MhSTIKI <arthawebid@gmail.com>
2025-11-05 18:52:37 +0800

    update json

--- laraproj/app/Http/Controllers/infoTemanController.php
+++ laraproj/app/Http/Controllers/infoTemanController.php
@@ -3,42 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class infoTemanController extends Controller
 {
-    private $dteman = [
-        [
-            "namateman"=>"Tangidah Idun",
-            "alamat"=>"Jl. Persatuan Kodok Blok F No 70",
-            "kota"=>"Denpasar",
-            'wa'=>"086677889",
-            'idbuku'=>1,
-        ],
-        [    
-            "namateman"=>"Rudiani Putri Dimana",
-            "alamat"=>"Jl. Tegak Lurus Blok T No 80",
-            "kota"=>"Denpasar",
-            "wa"=>"08774433267",
-            "idbuku"=>2,
-        ],
-    ];
+    private $dteman;
+
+    public function __construct(){
+        // membaca data json
+        $this->dteman = storage_path("app/private/dteman.json");
+    }
+
     public function index(){
         $nama = "Zizuki";
         
         //https://shorturl.at/6L1DX
 
         
-        $dtateman = $this->dteman;
+        $dtateman = $this->readData();
         return view('info', compact('nama','dtateman') );
     }
 
     public function detail($id){
-        $dtateman = collect($this->dteman)->firstWhere('idbuku',$id);
+        $dtateman = collect($this->readData())->firstWhere('idbuku',$id);
         
         if(!$dtateman){
             abort(404,"Data tidak ditemukan");
         }
         
         return view('infodetail',compact('dtateman'));
+    }
+
+    private function readData(){
+
+        if(!file_get_contents($this->dteman) ){
+            return [];
+        }
+
+        $jsn = file_get_contents($this->dteman);
+        $data = json_decode($jsn,true);
+        return $data ?: []; 
     }
 }
